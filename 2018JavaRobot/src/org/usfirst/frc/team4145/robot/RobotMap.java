ea7b610dc19e8e6cf4cf1fa5c363dbe91e63b8fd
coalman321 <coalman321@users.noreply.github.com>
2018-01-17 08:44:52 -0500

    added comments

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/RobotMap.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/RobotMap.java
@@ -29,11 +29,11 @@ public class RobotMap {
 	public static final String AutoList[] = {"Auto1","Auto2","Auto3","Auto4","Auto5",
 			"Auto6","Auto7","Auto8","Auto9","Auto10","Auto11","Auto12","DO NOTHING"}; // three different auto positions
 	//actuators
-	public static WPI_TalonSRX Drive1,Drive2,Drive3,Drive4;
-	public static MecanumDrive robotdrive;
+	public static WPI_TalonSRX Drive1,Drive2,Drive3,Drive4; //need to use WPI_talonSRX for drivetrain use
+	public static MecanumDrive robotdrive; //meccanum drive object
 	
 	//sensors
-	public static AHRS ahrs;
+	public static AHRS ahrs; //AHRS system on navx
 	public static Joystick stick1;
 	
 	//subsystems public static
@@ -45,14 +45,14 @@ public class RobotMap {
 		SmartDashboard.putStringArray("Auto Selector", AutoList); //publishes the auto list to the dashboard "Auto Selector"
 		
 		//all actuator objects here
-		Drive1 = new WPI_TalonSRX(1);
+		Drive1 = new WPI_TalonSRX(1); 
 		Drive2 = new WPI_TalonSRX(2);
 		Drive3 = new WPI_TalonSRX(3);
 		Drive4 = new WPI_TalonSRX(4);
-		robotdrive = new MecanumDrive(Drive1, Drive2, Drive3, Drive4);
+		robotdrive = new MecanumDrive(Drive1, Drive2, Drive3, Drive4); //create meccanum drive
 		
 		//all sensor objects here
-		ahrs = new AHRS(SPI.Port.kMXP);
+		ahrs = new AHRS(SPI.Port.kMXP); //finish declaring AHRS to MXP SPI bus
 		stick1 = new Joystick(0);
 		
 		//create all subsystem objects

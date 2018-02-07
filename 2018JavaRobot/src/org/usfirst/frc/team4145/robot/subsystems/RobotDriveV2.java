c95649981789407e5a8282ba407264b67b22ff00
coalman321 <coalman321@users.noreply.github.com>
2018-02-07 14:55:47 -0500

    added customPidSusbsystem and created RobotDrive version2

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/subsystems/RobotDriveV2.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/subsystems/RobotDriveV2.java
@@ -1,17 +1,125 @@
 package org.usfirst.frc.team4145.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc.team4145.robot.Robot;
+import org.usfirst.frc.team4145.robot.RobotMap;
+import org.usfirst.team4145.robot.shared.CustomPIDSubsystem;
 
-public class RobotDriveV2 extends Subsystem{
-	
-	
+/**
+ * Robot drive version 2
+ * this version of the drive system is built based on 0 - 360 from the ground up
+ * if you want the current gyro angle call getGyro()
+ */
 
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
+public class RobotDriveV2 extends CustomPIDSubsystem {
+
+    private PIDController gyroLock;
+
+    private boolean enLock = false;
+    private double deadBandVal = 0.35;
+    private double[] lastInputSet = {0, 0, 0};
+    private boolean isReversed = false;
+
+    //PID variables
+    private double Kp = 0.025;
+    private double Ki = 0.0;
+    private double Kd = 0.025;
+    private double absTol = 0.5;
+    private double pidOutput = 0;
+
+    public RobotDriveV2() {
+        gyroLock = new PIDController(Kp, Ki, Kd, this, this);
+        gyroLock.setAbsoluteTolerance(absTol);
+
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    @Override
+    public double pidRead() {
+        return getGyro();
+    }
+
+    @Override
+    public void pidWrite(double output) {
+        pidOutput = output;
+    }
+
+    public void periodic() {
+        if (!DriverStation.getInstance().isAutonomous()) lastInputSet = getAdjStick();
+        if (isReversed) {
+            lastInputSet[0] *= -1;
+            lastInputSet[1] *= -1;
+        }
+        SmartDashboard.putNumberArray("compensated stick values", lastInputSet);
+        SmartDashboard.putNumber("Gyro Target", gyroLock.getSetpoint());
+        SmartDashboard.putNumber("Gyro Angle", getGyro());
+        if (enLock) {
+            // Periodically updates while gyro locked
+            setCartesianDrive(lastInputSet[0], lastInputSet[1], pidOutput);
+
+        } else {
+            // periodically updates drive
+            setCartesianDrive(lastInputSet[0], lastInputSet[1], lastInputSet[2] / 1.25);
+            setTarget(getGyro()); // Safety feature in case PID gets enabled
+        }
+    }
+
+    public void enableTo(double rot, boolean en) {
+        this.setTarget(rot);
+        this.enableLock(en);
+    }
+
+    public void setInput(double[] inputvals) {
+        lastInputSet = inputvals;
+    }
+
+    public void flipRefrence(boolean isReversed) {
+        this.isReversed = isReversed;
+    }
+
+    public double getGyro() {
+        return (RobotMap.ahrs.getAngle() % 360);
+    }
+
+    private void setCartesianDrive(double x, double y, double z) {
+        RobotMap.robotdrive.driveCartesian(x, -y, z);
+    }
+
+    private void setTarget(double target) {
+        gyroLock.setSetpoint(target);
+    }
+
+    private void enableLock(boolean en) {
+        enLock = en;
+        if (enLock) gyroLock.enable();
+        else gyroLock.disable();
+    }
+
+    private double[] getAdjStick() {
+        double[] out = new double[3];
+        out[0] = evalDeadBand(Robot.oi.getMasterStick().getX(), deadBandVal);
+        out[1] = evalDeadBand(Robot.oi.getMasterStick().getY(), deadBandVal);
+        out[2] = evalDeadBand(Robot.oi.getMasterStick().getZ(), deadBandVal);
+        return out;
+    }
+
+    // figures out if the stick value is within the deadband
+    private double evalDeadBand(double stickInpt, double deadBand) {
+        if (Math.abs(stickInpt) < deadBand) {
+            return 0;
+        } else {
+            if (stickInpt < 0) {
+                return (0 - Math.pow(stickInpt, 2));
+            } else {
+                return Math.pow(stickInpt, 2);
+            }
+        }
+    }
 
 }
---
+++ 2018JavaRobot/src/org/usfirst/team4145/robot/shared/CustomPIDSubsystem.java
@@ -0,0 +1,29 @@
+package org.usfirst.team4145.robot.shared;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class CustomPIDSubsystem extends Subsystem implements PIDSource, PIDOutput{
+
+    public abstract double pidRead();
+
+    @Override
+    public abstract void pidWrite(double output);
+
+    @Override
+    public void setPIDSourceType(PIDSourceType pidSource) {
+
+    }
+
+    @Override
+    public PIDSourceType getPIDSourceType() {
+        return null;
+    }
+
+    public double pidGet(){
+        return pidRead();
+    }
+
+}

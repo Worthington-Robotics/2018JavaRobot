53bb5d2fd5b08738504c43cb67a66b286d97c8be
coalman321 <coalman321@users.noreply.github.com>
2018-02-08 23:28:21 -0500

    added example command group and auto command group package

---
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/ExampleGroup.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team4145.robot.autocommandgroups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class ExampleGroup extends CommandGroup {
+    public ExampleGroup(){
+
+    }
+}

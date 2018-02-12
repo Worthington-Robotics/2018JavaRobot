3e65edba9046192d19d1bcbcafde3405641a5dc2
fireflightg <fireflightwork@gmail.com>
2018-02-12 18:13:55 -0500

    added 2 autos revoved the extra

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/SaahibSwitch.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/SaahibSwitch.java
@@ -23,14 +23,5 @@ if(autonumber == 1 );
     	addSequential(new GyroToAngle(RobotMap.Drive.getGyro(-31.399)));
     	addSequential(new DriveTo(19*5));
     }
-if(autonumber == 2);
-    	{addParallel(new DriveTo(19*50));
-    	addParallel(new DropFork());
-    	addParallel(new GyroToAngle(RobotMap.Drive.getGyro()));
-    	addSequential(new GyroToAngle(RobotMap.Drive.getGyro()-90));
-    	addSequential(new DriveTo(19*40));
-    	addSequential(new GyroToAngle(RobotMap.Drive.getGyro()+90));
-    	addSequential(new DriveTo(19*10));
-}
     }
    }

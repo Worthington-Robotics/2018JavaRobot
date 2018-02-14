f6e0e9a53c5b2b04b12a8fd6b7d9f02bb877d547
fireflightg <fireflightwork@gmail.com>
2018-02-14 18:08:27 -0500

    mi swatch is fixed and corrected

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/SaahibSwitch.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/SaahibSwitch.java
@@ -1,27 +1,20 @@
 package org.usfirst.frc.team4145.robot.autocommandgroups;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.QueueGroup;
 
-public class SaahibSwitch extends CommandGroup {
-    public SaahibSwitch(int autonumber){
-    	
-    	
-if(autonumber == 0);
-    	
-    	{addParallel(new DriveTo(19*140));
-    	addParallel(new DropFork());
-    	addParallel(new GyroToAngle(RobotMap.Drive.getGyro()));
-    		
+public class SaahibSwitch extends QueueGroup {
+	public SaahibSwitch(int autonumber){
+		if(autonumber == 0){
+    		addParallel(new Command[]{new DriveTo(19 * 140), new DropForks()}, 1500);
+    		addSequential(new DropCube(), 200);
+		}
+		if(autonumber == 1 ){
+			addParallel(new Command[]{new DriveTo(19 * 0), new DropForks()}, 1500); 
+			addSequential(new GyroToAngle(31.399));
+			addParallel(new Command[]{new DriveTo(19 * 180.422), new DropForks()}, 1500);
+			addSequential(new GyroToAngle(-33.399));
+			addSequential(new DriveTo(19 * 5), 3000);
+			addSequential(new DropCube(), 200);
+		}
     }
-if(autonumber == 1 );
-    	
-    	{addParallel(new DriveTo(19*0));
-    	addParallel(new DropFork());
-    	addParallel(new GyroToAngle(RobotMap.Drive.getGyro()));
-    	addSequential(new GyroToAngle(RobotMap.Drive.getGyro()+31.399));
-    	addSequential(new DriveTo(19*180.422));
-    	addSequential(new GyroToAngle(RobotMap.Drive.getGyro(-31.399)));
-    	addSequential(new DriveTo(19*5));
-    }
-    }
-   }
+}

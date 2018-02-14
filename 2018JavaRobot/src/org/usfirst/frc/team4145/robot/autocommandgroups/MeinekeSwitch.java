f5f205e7885a68f27b8ef3788473772b4d0d6295
mmeineke1014 <35080951+mmeineke1014@users.noreply.github.com>
2018-02-14 16:44:12 -0500

    updated formating

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/MeinekeSwitch.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/MeinekeSwitch.java
@@ -1,27 +1,27 @@
 package org.usfirst.frc.team4145.robot.autocommandgroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team4145.robot.commands.autoonly.*;
+import org.usfirst.team4145.robot.shared.QueueGroup;
 
 public class MeinekeSwitch extends CommandGroup {
 	public MeinekeSwitch(int autonomous) {
 
 		if (autonomous == 0) {
-			addParallel(new DriveTo(19 * 109)); // forward 109 inches
-			addParallel(new LiftToPostiton(1000));//raise lift 
-			addSequential(new DropForks());//drop forks
-			addSequential(new GyroToAngle(-15));// turn left 15 degrees
-			addSequential(new DropCube()); // launch cube
+			//Forward 109 inches, raise lift, drop forks
+			addParallel(new Command[] {new DriveTo(19*109), new liftToPosition(1000), new DropForks()}, 2500);
+			addSequential(new GyroToAngle(-15), 1500);// turn left 15 degrees
+			addSequential(new DropCube(), 500); // launch cube 
 		}
 		else
 		{
-			addSequential(new GyroToAngle(10));//turn right 10 degrees
-			addSequential(new DriveTo(19*199));//forward 199 inches
-			addSequential(new GyroToAngle(-90));//turn left 90 degrees
-			addParallel(new LiftToPosition(1000));//raise lift
-			addParallel(new DriveTo(19*189));//forward 189 inches
-			addSequential(new DropForks());//drop forks
-			addSequential(new GyroToAngle(-90));//turn left 90 degrees
-			addSequential(DropCube());//launch cube
+			addSequential(new GyroToAngle(10), 1500);//turn right 10 degrees
+			addSequential(new DriveTo(19*199), 2000);//forward 199 inches
+			addSequential(new GyroToAngle(-90), 2000);//turn left 90 degrees
+			// Forward 189 inches, raise lift, drop forks
+			addParallel(new Command[] {new DriveTo(19*189), new liftToPosition(1000), new DropForks()}, 2000);
+			addSequential(new GyroToAngle(-90), 2000);//turn left 90 degrees
+			addSequential(DropCube(), 500);//launch cube 
 		}
 	}
 }

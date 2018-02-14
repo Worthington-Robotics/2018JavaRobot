907eede6f5a68294e1ad1d9da223d5feb460b900
Milo Browne <milobrowne2020@gmail.com>
2018-02-14 18:48:52 -0500

    added new system is broke tho

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/Postion1Scale.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/Postion1Scale.java
@@ -1,47 +1,26 @@
 package org.usfirst.frc.team4145.robot.autocommandgroups;
 
-import org.usfirst.frc.team4145.robot.RobotMap;
-import org.usfirst.frc.team4145.robot.commands.autoonly.DriveTo;
-import org.usfirst.frc.team4145.robot.commands.autoonly.DropCube;
-import org.usfirst.frc.team4145.robot.commands.autoonly.DropForks;
-import org.usfirst.frc.team4145.robot.commands.autoonly.GyroToAngle;
-import org.usfirst.frc.team4145.robot.commands.autoonly.HighLiftUp;
-import org.usfirst.frc.team4145.robot.commands.autoonly.LiftToPosition;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.QueueGroup;
 
-public class Postion1Scale extends CommandGroup {
+public class Postion1Scale extends QueueGroup {
     public Postion1Scale(int autonumber){
     	if(autonumber == 0){ //If it is the 1st auto assignment
-    		addParallel(new DriveTo(19*299.65)); //drive 299.65 inches to Scale
-    		addParallel(new GyroToAngle(RobotMap.drive.getGyro())); //Actually drive straight forward
-    		addParallel(new DropForks()); //what it says on the tin
-    		addParallel(new LiftToPosition(1000)); //Lift Stage 1 (the lower bit), Placeholder number
+    		addParallel(new Command[]{new DriveTo(19*299.65)), new GyroToAngle(RobotMap.drive.getGyro()), new DropForks(), new LiftToPosition(1000)});
     		
-    		addSequential(new GyroToAngle(RobotMap.drive.getGyro()+90)); //Turn 90 degrees
-    		addParallel(new HighLiftUp()); //Lift up top bit
+    		addParallel(new Command[]{GyroToAngle(RobotMap.drive.getGyro()+90), HighLiftUp()});
     		
-    		addSequential(new DropCube); //off loads cube
+    		addSequential(new DropCube);
     		
-    		addSequential(HighLiftDown()); //Lowers high bit
-    		addParallel(new LiftToPosition(0)); //Lowers low bit
-    		addParallel(newGyroToAngle(RobotMap.drive.getGyro()+90)); //Finishes turn around
-    		
-    		//addSequential(new DriveTo(19*103.65)); //drive 103.65 inches to Switch
+    		addParallel(new Command[]{HighLiftDown()), new LiftToPosition(0), new GyroToAngle(RobotMap.drive.getGyro()+90)});
     	}
     	if(autonumber == 1){
-    		addParallel(new DriveTo(19*228.735)); //drive 228.735 inches
-    		addParallel(new GyroToAngle(RobotMap.drive.getGyro())); //Actually drive straight forward
-    		addParallel(new DropForks()); //what it says on the tin
-    		addParallel(new LiftToPosition(1000)); //Lift Stage 1 (the lower bit), Placeholder number
+    		addParallel(new Command[]{new DriveTo(19*228.735)), addSequential( GyroToAngle(), new DropForks(), new LiftToPosition(1000)};
     		
-    		addsequential(new GyroToAngle(RobotMap.drive.getGyro()+90)); //Turn 90 degrees
+    		addParallel(new Command[]{new GyroToAngle(RobotMap.drive.getGyro()+90), new DriveTo(19*190)});
     		
-    		addParallel(new DriveTo(19*190)); //drive 190 inches
-    		
-    		addsequential(new GyroToAngle(RobotMap.drive.getGyro()-90)); //Turn 90 degrees the other direction
-    		addParallel(new DriveTo(19*70.915)); //Drive 70.915 inches
-    		addParallel(new HighLiftUp()); //Lift up top bit
+    		addParallel(new Command[]{new GyroToAngle(RobotMap.drive.getGyro()-90), new DriveTo(19*70.915), new HighLiftUp()}); 
     	}
     }
 }
+ 

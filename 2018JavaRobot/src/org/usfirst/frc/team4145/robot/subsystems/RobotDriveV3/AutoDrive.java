627c7b3dbf06dc3179709d6d7f0f3accd70c4ca8
coalman321 <coalman321@users.noreply.github.com>
2018-03-04 21:56:10 -0500

    reformatted Autodrive and added comments and other tuning values

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/subsystems/RobotDriveV3/AutoDrive.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/subsystems/RobotDriveV3/AutoDrive.java
@@ -5,24 +5,26 @@ import edu.wpi.first.wpilibj.PIDController;
 public class AutoDrive {
 
     //data objects / fields
-    private PIDController leftVelocity;
-    private PIDController rightVelocity;
+    private PIDController leftVelocity; //may be unnecessary
+    private PIDController rightVelocity; //may be unnecessary
     private double[] outputArray = {0,0};
 
     //Shared PID Constants
-    private double VELOCITY_PROPORTIONAL = 0.0000;
-    private double MAX_VELOCITY = 4.0000; //counts per second
-    private double MAX_ACELLERATION = 2.0000; //counts per second per second
-
+    private double VELOCITY_PROPORTIONAL   = 0.0000; //proportional scalar between motor power level and velocity output
+    private double MAX_VELOCITY            = 4.0000; //counts per second
+    private double MAX_ACELLERATION        = 2.0000; //counts per second per second
 
     //Left PID constants
-    private double LEFT_PROPORTIONAL_GAIN = 0.0000;
-    private double LEFT_INTEGRAL_GAIN = 0.0000;
-    private double LEFT_DERIVATIVE_GAIN = 0.0000;
-    private double LEFT_FEEDFORWARD = 0.0000;
-
+    private double LEFT_PROPORTIONAL_GAIN  = 0.0000;
+    private double LEFT_INTEGRAL_GAIN      = 0.0000;
+    private double LEFT_DERIVATIVE_GAIN    = 0.0000;
+    private double LEFT_FEEDFORWARD        = 0.0000; //may be unnecessary
 
     //Right PID constants
+    private double RIGHT_PROPORTIONAL_GAIN = 0.0000;
+    private double RIGHT_INTEGRAL_GAIN     = 0.0000;
+    private double RIGHT_DERIVATIVE_GAIN   = 0.0000;
+    private double RIGHT_FEEDFORWARD       = 0.0000; //may be unnecessary
 
     AutoDrive(){
         //TODO finish class declaration

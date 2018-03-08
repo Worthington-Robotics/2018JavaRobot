7c8107c201d86b2561cac31f21a416e11514be09
fireflightg <fireflightwork@gmail.com>
2018-03-08 18:40:28 -0500

    added new auto system

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/CrossLineLong.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/CrossLineLong.java
@@ -1,10 +1,11 @@
 package org.usfirst.frc.team4145.robot.autocommandgroups;
 
 import org.usfirst.frc.team4145.robot.commands.autoonly.DriveTo;
-import org.usfirst.frc.team4145.robot.shared.AutoStateMachine.QueueGroup;
+import org.usfirst.frc.team4145.robot.shared.QueueGroup;
 
 public class CrossLineLong extends QueueGroup {
     public CrossLineLong(){
-        addSequential(new DriveTo(19 * 204), 2000);
+        addDrive("CrossLineLong_left.csv","CrossLineLong_right.csv")
+    	addSequential(2000);
     }
 }
--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/Position1Switch.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/Position1Switch.java
@@ -9,17 +9,15 @@ import edu.wpi.first.wpilibj.command.Command;
 public class Position1Switch extends QueueGroup {
 	public Position1Switch(int autonumber){
 		if(autonumber == 0){
-    		addParallel(new Command[]{new DriveTo(19 * 194),new LiftToPosition(1000)}, 3000);
-    		addSequential(new GyroToAngle(90), 1000);
+    		addDrive("Position1Switch0_left.csv","Position1Switch0_right.csv");
+			addParallel(new Command[]{new LiftToPosition(1000)}, 3000);
     		addSequential(new DropForks(), 1000);
     		addSequential(new DropCube(false), 1000);
 		}
 		if(autonumber == 1 ){
-			addParallel(new Command[]{new DriveTo(19 * 200)}, 2000); 
-			addSequential(new GyroToAngle(90), 1000);
-			addParallel(new Command[]{new DriveTo(19 * 154),new LiftToPosition(1000)}, 4000);
-			addSequential(new GyroToAngle(90), 1000);
-			addParallel(new Command[] {new DriveTo(19 * 5), new DropForks()}, 3000);
+			addDrive("PositionSwith1_left.csv","Position1Switch1_right.csv"); 
+			addParallel(new Command[]{new LiftToPosition(1000)}, 4000);
+			addParallel(new Command[] {new DropForks()}, 3000);
 			addSequential(new DropCube(false), 1000);
 		}
     }

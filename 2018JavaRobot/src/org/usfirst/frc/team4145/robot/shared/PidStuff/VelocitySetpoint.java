c24a98fd28ee47afa1dce3ff7bae7832750b3f95
Phillip White <boring.someone@gmail.com>
2018-03-08 16:42:28 -0500

    added velocitysetpoint array builder

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/shared/PidStuff/VelocitySetpoint.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/shared/PidStuff/VelocitySetpoint.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4145.robot.shared.PidStuff;
-
+import java.util.Scanner;
+import java.io.file;
 public class VelocitySetpoint {
 
     private double dist, velo, acc;
@@ -18,5 +19,41 @@ public class VelocitySetpoint {
 
     public double getVelocity(){
         return velo;
+    }
+    
+    public static VelocitySetpoint[] pointArray(String x)
+    {
+    	try {
+    	File CSV = new File(x);}
+    	catch(IOExeption error)
+    	{}
+    	int i = 0;
+    	int a = 0;
+    	int v = 0;
+    	int d = 0;
+    	Scanner in = new Scanner(CSV);
+    	in.setDelimiter(',');
+    	VelocitySetpoint [] pointArray = new VelocitySetpoint [3000];
+    	while(in.hasNext())
+    	{
+    		if(i%3 == 0)
+    		{
+    	VelocitySetpoint point = new VelocitySetpoint(d,v,a);
+    	pointArray[i/3] = point;
+    	d = in.nextInt();
+    	i++;
+    		}
+    		if(i%3 == 1)
+    		{
+    	v = in.nextInt();
+    	i++;
+    		}
+    		if(i%3 == 2)
+    		{
+    	a = in.nextInt();
+    	i++;
+    		}
+    	}
+    		
     }
 }

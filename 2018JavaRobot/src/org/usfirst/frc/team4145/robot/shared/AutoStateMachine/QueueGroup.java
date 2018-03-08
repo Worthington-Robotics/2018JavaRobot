d39d3a76d1a412478ee10c59c9177bc19dff209b
Phillip White <boring.someone@gmail.com>
2018-03-08 17:41:14 -0500

    Added more support to QueueGroup by VelocitySetpoint

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/shared/AutoStateMachine/QueueGroup.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/shared/AutoStateMachine/QueueGroup.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4145.robot.shared.AutoStateMachine;
 
 import edu.wpi.first.wpilibj.command.Command;
+import src.org.usfirst.frc.team4145.robot.shared.PidStuff.VelocitySetpoint;
 
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -15,6 +16,7 @@ import static java.util.Objects.requireNonNull;
 public class QueueGroup {
 
     private Queue<CommandQueueGroup> queuedStates;
+    private VelocitySetpoint [] RightDrive, LeftDrive;
 
     public QueueGroup() {
         queuedStates = new LinkedBlockingQueue<>();
@@ -30,6 +32,23 @@ public class QueueGroup {
         requireNonNull(commands, "Command cannot be null");
         queuedStates.add(new CommandQueueGroup(commands, timeOutMs));
     }
+    
+    protected void addDrive(String leftDrive, String rightDrive, long timeOutMs) {
+        requireNonNull(leftDrive, "left file path cannot be null");
+        requireNonNull(rightDrive, "right file path cannot be null");
+        LeftDrive = VelocitySetpoint.pointArray(leftDrive);
+        RightDrive = VelocitySetpoint.pointArray(rightDrive);
+        
+        
+    }
+    
+    public VelocitySetpoint[] getRightDrive()
+    {return RightDrive;}
+    
+    public VelocitySetpoint[] getRightDrive()
+    {return LeftDrive;}
+    
+
 
     public LinkedBlockingQueue<CommandQueueGroup> getQueuedStates() {
         return (LinkedBlockingQueue<CommandQueueGroup>) queuedStates;

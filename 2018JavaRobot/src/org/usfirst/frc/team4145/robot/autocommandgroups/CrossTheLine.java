4dc50d64b421fe9efb6c7ea5e13d4217a930e86e
markcfong561 <35080862+markcfong561@users.noreply.github.com>
2018-03-08 18:31:05 -0500

    Did the addDrive stuff

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/CrossTheLine.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/autocommandgroups/CrossTheLine.java
@@ -5,7 +5,7 @@ import org.usfirst.frc.team4145.robot.shared.AutoStateMachine.QueueGroup;
 
 public class CrossTheLine extends QueueGroup {
     public CrossTheLine(){
-        addSequential(new DriveTo(19 * 102), 1500);
+        addDrive("CrossTheLine_left.csv", "CrossTheLine_right.csv");
     }
 
 }

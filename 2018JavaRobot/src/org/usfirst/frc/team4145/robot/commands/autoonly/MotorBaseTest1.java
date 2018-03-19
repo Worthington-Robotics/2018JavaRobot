a70c3f545b23bd0bd07ed45e405d147a9064e30d
Phillip White <boring.someone@gmail.com>
2018-03-19 16:07:49 -0400

    Update 767043

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/commands/autoonly/MotorBaseTest1.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/commands/autoonly/MotorBaseTest1.java
@@ -16,7 +16,7 @@ public class MotorBaseTest1 extends Command {
 
 	@Override
 	protected boolean isFinished() {
-		return iterations > 1750;
+		return iterations > 705;
 	} // has to finish at end of auto routine
 
 	public void execute() {
@@ -34,11 +34,15 @@ public class MotorBaseTest1 extends Command {
 			RobotMap.driveRearLeft.setSpeed(.254);}
 		if(iterations < 175 && iterations > 150)
 		{RobotMap.driveRearLeft.setSpeed(0);}
+		if(iterations > 175 && iterations < 702)
+		{RobotMap.driveFrontRight.setSpeed(.5);
+		RobotMap.driveFrontLeft.setSpeed(.5);
+		}
 	}
 
 	public void end() {
 		
-		SmartDashboard.putBoolean("Lower Lift Test", Test1pt1 && Test1pt2);
+		SmartDashboard.putBoolean("Motor Check Test", RobotMap.rightWheelEncoder.getDistance() / 10 == 10 && RobotMap.leftWheelEncoder.getDistance() / 10 == 10);
 	}
 
 	public void interrupted() {

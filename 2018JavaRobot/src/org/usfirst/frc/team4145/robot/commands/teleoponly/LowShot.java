1c867057cdedb1195f6edce10752375a685f0f6b
coalman321 <coalman321@users.noreply.github.com>
2018-03-22 07:17:57 -0400

    clean repo

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/commands/teleoponly/LowShot.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team4145.robot.commands.teleoponly;
-
-import org.usfirst.frc.team4145.robot.commands.autoonly.DropCube;
-
-public class LowShot extends DropCube {
-    public LowShot(){
-        super(false);
-    }
-}

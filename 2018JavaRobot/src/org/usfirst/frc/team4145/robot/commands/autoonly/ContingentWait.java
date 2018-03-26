a84e5950d2c5a582e69018216f25d94b99a43c52
coalman321 <coalman321@users.noreply.github.com>
2018-03-26 11:51:57 -0400

    added wait and contingent wait

---
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/commands/autoonly/ContingentWait.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team4145.robot.commands.autoonly;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+
+public class ContingentWait extends Command {
+    public ContingentWait() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    protected void initialize() {
+
+    }
+
+    protected void execute() {
+
+    }
+
+    protected boolean isFinished() {
+        // TODO: Make this return true when this Command no longer needs to run execute()
+        return false;
+    }
+
+
+    protected void end() {
+
+    }
+
+    protected void interrupted() {
+        end();
+    }
+}
---
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/commands/autoonly/Wait.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4145.robot.commands.autoonly;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Wait extends Command {
+
+    @Override
+    protected boolean isFinished() {
+         return false;
+    }
+}

0a55332fabcc8d55fa41ad281dedfc9dd13ae46f
coalman321 <coalman321@users.noreply.github.com>
2018-03-29 07:35:11 -0400

    removed extraneous printlines

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/shared/AutoStateMachine/AutoStateMachine.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/shared/AutoStateMachine/AutoStateMachine.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4145.robot.shared.AutoStateMachine;
 
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team4145.robot.commands.autoonly.ExecuteMotionProfile;
 import org.usfirst.frc.team4145.robot.shared.HardwareTimer;
@@ -17,23 +18,23 @@ public class AutoStateMachine {
     private static Runnable taskRunnable = () -> {
     	autoState = 0;
         if (blockingQueue != null) {
-            System.out.println("State machine size:" + blockingQueue.size());
+            //System.out.println("State machine size:" + blockingQueue.size());
             SmartDashboard.putString("Auto State Machine Status", "State machine preparing to start!");
             //System.out.println("blocking queue elements:" + blockingQueue.toArray());
             while (blockingQueue.iterator().hasNext()) {
-                System.out.println("Entering state " + autoState + " At time " + RobotController.getFPGATime());
+                //System.out.println("Entering state " + autoState + " At time " + RobotController.getFPGATime());
                 SmartDashboard.putNumber("Auto State", autoState);
                 try {
                     inspectedElement = blockingQueue.take();
                     if(!(inspectedElement.getQueueGroup().peek() instanceof ExecuteMotionProfile) && autoState == 0){ //check to see if first command is motion profile
                         SmartDashboard.putString("Auto State Machine Status", "No motion profile fed in state zero");
-                        System.out.println("State zero did not contain a motion profile");
+                        //System.out.println("State zero did not contain a motion profile");
                     }
                     inspectedElement.startQueueGroup(); //starts queue group running
                     while (!inspectedElement.checkQueueGroup()) { //checks status of state and whether it is or should be dead
                         //System.out.println("Waiting for previous task to die");
                         SmartDashboard.putString("Auto State Machine Status", "Waiting for previous task to die");
-                        HardwareTimer.hardwareSleep(10);
+                        Timer.delay(0.010);
                     }
                     //SmartDashboard.putNumber("State Advance Flag",1);
                     inspectedElement.killQueueGroup(); //forcefully kills group (just in case)
@@ -46,10 +47,10 @@ public class AutoStateMachine {
                 autoState++; //increment auto state
             }
 
-            System.out.println("Finished queue");
+            //System.out.println("Finished queue");
             SmartDashboard.putString("Auto State Machine Status", "State machine finished Queue");
         } else {
-            System.out.println("State machine list was null!");
+            //System.out.println("State machine list was null!");
             SmartDashboard.putNumber("Auto State", -2);
             SmartDashboard.putString("Auto State Machine Status", "List was NULL!");
         }
@@ -59,7 +60,7 @@ public class AutoStateMachine {
         blockingQueue = queueGroups;
         Notifier monitor = new Notifier(taskRunnable);
         //monitor.setPriority(8);
-        System.out.println("Starting Machine thread now! at time " + RobotController.getFPGATime());
+        System.out.println("Starting Machine thread at time " + RobotController.getFPGATime());
         monitor.startSingle(0.000);
     }
 

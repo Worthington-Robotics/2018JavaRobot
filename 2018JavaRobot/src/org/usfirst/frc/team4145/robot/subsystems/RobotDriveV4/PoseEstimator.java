b0273d41b560cb1fbc5d833d9f4d8fcc6e6d5e67
coalman321 <coalman321@users.noreply.github.com>
2018-04-18 10:03:07 -0400

    remmoved test transform

--- 2018JavaRobot/src/org/usfirst/frc/team4145/robot/subsystems/RobotDriveV4/PoseEstimator.java
+++ 2018JavaRobot/src/org/usfirst/frc/team4145/robot/subsystems/RobotDriveV4/PoseEstimator.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class PoseEstimator {
 
     private Notifier m_NotifierInstance;
-    protected InterpolatingTreeMap<InterpolatingDouble, RigidTransform2d> fieldToVehicle, test;
+    protected InterpolatingTreeMap<InterpolatingDouble, RigidTransform2d> fieldToVehicle;
     protected double leftPrevEncCount = 0;
     protected double rightPrevEncCount = 0;
     protected static final RigidTransform2d encodersToVehicle = new RigidTransform2d(new Translation2d(Constants.ENCODER_X_OFFSET, Constants.ENCODER_Y_OFFSET),
@@ -28,7 +28,6 @@ public class PoseEstimator {
 
     public void reset(double start_time, RigidTransform2d initial_field_to_vehicle){
         fieldToVehicle = new InterpolatingTreeMap<>(Constants.OBSERVATION_BUFFER_SIZE);
-        test = new InterpolatingTreeMap<>(Constants.OBSERVATION_BUFFER_SIZE);
         fieldToVehicle.put(new InterpolatingDouble(start_time), initial_field_to_vehicle);
         leftPrevEncCount = 0;
         rightPrevEncCount = 0;
@@ -38,9 +37,6 @@ public class PoseEstimator {
         return fieldToVehicle.lastEntry();
     }
 
-    public synchronized Map.Entry<InterpolatingDouble, RigidTransform2d> getLatestTest(){
-        return test.lastEntry();
-    }
 
     private Runnable loop = () -> {
         double currentTime = Timer.getFPGATimestamp();
@@ -48,8 +44,12 @@ public class PoseEstimator {
         double currentRightEncoder = RobotMap.robotDriveV4.getRightEncoder() * ((Constants.WHEEL_DIAMETER * 3.14159) / Constants.COUNTS_PER_REV);
         Rotation2d gyro = Rotation2d.fromDegrees(RobotMap.robotDriveV4.getGyroContinuous());
         RigidTransform2d odometry = generateOdometryFromSensors((currentLeftEncoder - leftPrevEncCount), (currentRightEncoder - rightPrevEncCount), gyro);
+        SmartDashboard.putString("Odometry", odometry.toString());
+        SmartDashboard.putString("Inverse Odometry", odometry.inverse().toString());
         RigidTransform2d transOdometry = odometry.transformBy(encodersToVehicle); //transforms to encoder frame of refrence - may need inverse
-        addObservations(currentTime, odometry, transOdometry);
+        SmartDashboard.putString("Trans Odometry", transOdometry.toString());
+        SmartDashboard.putString("Inverse Trans Odometry", transOdometry.inverse().toString());
+        addObservations(currentTime, odometry);
         outputToSmartDashboard();
         leftPrevEncCount = currentLeftEncoder;
         rightPrevEncCount = currentRightEncoder;
@@ -64,21 +64,12 @@ public class PoseEstimator {
         fieldToVehicle.put(new InterpolatingDouble(timestamp), observation);
     }
 
-    public synchronized void addObservations(double timestamp, RigidTransform2d observation, RigidTransform2d testTransform) {
-        fieldToVehicle.put(new InterpolatingDouble(timestamp), observation);
-        test.put(new InterpolatingDouble(timestamp), testTransform);
-    }
 
     public void outputToSmartDashboard() {
         RigidTransform2d odometry = getLatestFieldToVehicle().getValue();
         SmartDashboard.putNumber("Robot Pose X", odometry.getTranslation().getX());
         SmartDashboard.putNumber("Robot Pose Y", odometry.getTranslation().getY());
         SmartDashboard.putNumber("Robot Pose Theta", odometry.getRotation().getDegrees());
-
-        RigidTransform2d testOdom = getLatestTest().getValue();
-        SmartDashboard.putNumber("Robot Pose X2", testOdom.getTranslation().getX());
-        SmartDashboard.putNumber("Robot Pose Y2", testOdom.getTranslation().getY());
-        SmartDashboard.putNumber("Robot Pose Theta2", testOdom.getRotation().getDegrees());
     }
 
 }
